52339d2296e92dbff1b96be47798c9321622e54c
hernandw <hernandw@gmail.com>
2022-01-22 10:54:14 -0300

    pagina de ejemplo

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 /* require('dotenv').config({path: 'variables.env' }) */
 const PORT = process.env.PORT || 3000
+const path = require('path')
 
 app.listen(PORT, ()=>{
     console.log(`Servidor encendido on port: ${PORT}`);
 });
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) =>{
+    res.send('Mi respuesta desde express')
+})
 
