e23ff3e44f9cba43a6d7dd501970209a9202b7cc
Williams Hernandez <hernandw@gmail.com>
2022-01-22 10:29:03 -0300

    cambiamos el puerto

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-require('dotenv').config({path: 'variables.env' })
-const PORT = process.env.PORT || 5000
+/* require('dotenv').config({path: 'variables.env' }) */
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, ()=>{
     console.log(`Servidor encendido on port: ${PORT}`);
