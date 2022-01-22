3df5648dd66da325d115410a56bc4867770a3b79
Williams Hernandez <hernandw@gmail.com>
2022-01-22 11:59:11 -0300

    creacion de views

--- app.js
+++ app.js
@@ -7,15 +7,21 @@ const path = require('path')
 app.listen(PORT, ()=>{
     console.log(`Servidor encendido on port: ${PORT}`);
 });
+//Motor de Plantilla
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
-app.use(express.static(__dirname + 'public'));
+
 
 
 app.get('/', (req, res) =>{
-    res.send('Mi respuesta desde express')
+    res.render('index', {
+        titulo: "Pagina Principal"
+    })
 })
 
 app.get('/servicios', (req, res) =>{
     res.send('pagina de servicios')
 })
+app.use(express.static(__dirname + '/public'));
 
